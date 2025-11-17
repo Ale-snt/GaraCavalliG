aea1ad791362d2cf103249830ca0583e5d0495a9
Ale-snt <santonialessio2@gmail.com>
2025-11-17 20:49:15 +0100

    Update GaraCavalli.java

--- GaraCavalli.java
+++ GaraCavalli.java
@@ -1,83 +1,78 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.io.FileWriter;
+import java.io.IOException;
+import javax.swing.JFileChooser;
 
-// Classe Cavallo che estende Thread
 class Cavallo extends Thread {
-    private final String nome;       
-    private final int percorso;     
-    private int posizione = 0;       
-
-    // Lista condivisa tra tutti i cavalli per salvare l'ordine d'arrivo
-    private static final List<String> classifica = Collections.synchronizedList(new ArrayList<>());
-
+    private String nome;
+    private int percorso;
+    private int posizione = 0;
+    private static List<String> classifica = new ArrayList<>();
+    
     public Cavallo(String nome, int percorso) {
         this.nome = nome;
         this.percorso = percorso;
     }
 
-    @Override
     public void run() {
+        Random random = new Random();
+        
         while (posizione < percorso) {
-            posizione += (int)(Math.random() * 10) + 1; // avanza da 1 a 10 metri
-            System.out.println(nome + " ha percorso " + posizione + " metri");
-
-            try {
-                Thread.sleep((int)(Math.random() * 400) + 100); // pausa casuale
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-
-            // Se il cavallo ha finito il percorso, lo aggiungiamo in classifica
-            if (posizione >= percorso && !classifica.contains(nome)) {
+            posizione += random.nextInt(10) + 1;
+            System.out.println(nome + " è a " + posizione + " metri");
+            
+            if (posizione >= percorso) {
+                System.out.println(nome + " ha finito!");
                 classifica.add(nome);
-                System.out.println("\n🏁 " + nome + " ha tagliato il traguardo!");
+                break;
             }
+            
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {}
         }
     }
-
-    // Metodo statico per accedere alla classifica finale
+    
     public static List<String> getClassifica() {
         return classifica;
     }
 }
 
 public class GaraCavalli {
-    public static void main(String[] args) {
-        Scanner tastiera = new Scanner(System.in);
-        System.out.print("Inserisci la lunghezza del percorso in metri: ");
-        int lunghezza = tastiera.nextInt();
+    public static void main(String[] args) throws InterruptedException, IOException {
+        JFileChooser chooser = new JFileChooser();
+        chooser.setDialogTitle("Scegli dove salvare la classifica");
+        int scelta = chooser.showSaveDialog(null);
 
-        // Creiamo i cavalli
-        Cavallo c1 = new Cavallo("Ale", lunghezza);
-        Cavallo c2 = new Cavallo("Alexa", lunghezza);
-        Cavallo c3 = new Cavallo("Toro", lunghezza);
-        Cavallo c4 = new Cavallo("Giulia", lunghezza);
-        Cavallo c5 = new Cavallo("Canto", lunghezza);
-
-        // Avviamo la gara
-        c1.start();
-        c2.start();
-        c3.start();
-        c4.start();
-        c5.start();
-
-        // Aspettiamo che tutti finiscano
-        try {
-            c1.join();
-            c2.join();
-            c3.join();
-            c4.join();
-            c5.join();
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
+        if (scelta != JFileChooser.APPROVE_OPTION) {
+            System.out.println("Hai annullato. Addio!");
+            return;
         }
 
-        // Stampiamo la classifica finale (dal primo all’ultimo)
-        System.out.println("\n=== 🏆 CLASSIFICA FINALE 🏆 ===");
+        String percorsoFile = chooser.getSelectedFile().getAbsolutePath();
+        System.out.println("Salvo in: " + percorsoFile);
+
+        int lunghezza = 50;
+        ArrayList<Cavallo> cavalli = new ArrayList<>();
+        cavalli.add(new Cavallo("Marco", lunghezza));
+        cavalli.add(new Cavallo("Luca", lunghezza));
+        cavalli.add(new Cavallo("Anna", lunghezza));
+        cavalli.add(new Cavallo("Giulia", lunghezza));
+
+        for (Cavallo c : cavalli) c.start();
+        for (Cavallo c : cavalli) c.join();
+        
         List<String> classifica = Cavallo.getClassifica();
-        for (int i = 0; i < classifica.size(); i++) {
-            System.out.println((i + 1) + "° posto: " + classifica.get(i));
+
+        try (FileWriter writer = new FileWriter(percorsoFile)) {
+            writer.write("Classifica finale:\n");
+            for (int i = 0; i < classifica.size(); i++) {
+                writer.write((i + 1) + ". " + classifica.get(i) + "\n");
+            }
         }
 
-        tastiera.close();
+        System.out.println("\nClassifica salvata nel file!");
     }
 }
